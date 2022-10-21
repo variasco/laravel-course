fd4c116988607909cff18c234b4e0fee46397439
variasco <vakin213@gmail.com>
2022-10-21 19:41:28 +0300

    added some pages

---
+++ resources/views/welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Welcome page</title>
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    
+    <body>
+        <h1>Welcome page<h1>
+    </body>
+</html>
